24f125e09d49774cb0b53578a1cd174b33f469aa
alistree <a.saghiran@gmail.com>
2015-12-11 23:55:46 +0100

    test breaklist

--- Project-Emul-ARM-V7/Emul-Arm/test/test_breakpoint_list/test.c
+++ Project-Emul-ARM-V7/Emul-Arm/test/test_breakpoint_list/test.c
@@ -2,6 +2,33 @@
 #include <string.h>
 #include "breakpoint.h"
 
+
+
+void  _breakcmd_del(BREAKPOINT* list , unsigned int adr){
+  BREAKPOINT l=*list;
+  BREAKPOINT p;
+
+  if (l->stop_adr == adr){
+    *list = dehead_list_break(*list);
+    printf("adresse 0x%X supprimée\n",adr);
+    return ;
+  }
+    
+  while((l->nxt)!=NULL && ((l->nxt)->stop_adr != adr)){
+    l = l->nxt;
+    }
+
+ if(l->nxt== NULL){
+      puts("liste des points d'arret est vide ou term inexistant");
+      return ;
+    }
+    p = l->nxt;
+    p = dehead_list_break(p);
+    l->nxt = p;
+    printf("adresse 0x%X supprimée\n",adr);
+    return ;
+}
+
 int main (void){
   unsigned int adr = 0 ;
   BREAKPOINT mylist = NULL;
@@ -28,11 +55,16 @@ int main (void){
   affiche_list(mylist);
 
 
-  mylist = dehead_list_break(mylist);
-  affiche_list(mylist);
+  //mylist = dehead_list_break(mylist);
+  //affiche_list(mylist);
   mylist = depile_list_break(mylist);
   affiche_list(mylist);
 
+
+  puts("* break del *");
+  scanf("%x",&adr);
+  _breakcmd_del(&mylist,adr);
+  affiche_list(mylist);
 
   puts("\n libération");
   mylist = free_break_list(mylist);
